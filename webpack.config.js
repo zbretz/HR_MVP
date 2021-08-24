2143478124c0e3fef621a31a2b6e658f44dfabea
Zach <zachary.bretz@gmail.com>
2021-08-23 19:40:34 -0700

    Added gitignore and webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+//SO solution to an error thrown due to contentBase:
+// https://stackoverflow.com/questions/67926476/webpack-dev-server-config-contentbase-not-working-in-latest-version
+
+// module.exports = {
+//   // 1
+//   entry: './src/index.js',
+//   // 2
+//   output: {
+//     path: '/dist',
+//     filename: 'bundle.js'
+//   },
+//   // 3
+//   devServer: {
+//     contentBase: './dist'
+//   }
+// };
+
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+  },
+};
