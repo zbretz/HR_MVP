d29154ae0ef5a8f3ad733c0b6fa2c7382cdc880c
Zach <zachary.bretz@gmail.com>
2021-08-25 09:24:17 -0700

    Installed react and now serving rendered components

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,9 @@
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React with Webpack and Babel';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // res.send('Hello World!');
 });
 
 app.listen(3000, () =>
