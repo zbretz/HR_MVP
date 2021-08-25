5d99c4c49be109f15850b7a85cd34d0ff78255d9
Zach <zachary.bretz@gmail.com>
2021-08-24 21:51:10 -0700

    Added command to create test Questions

--- database/models/qa_commands.js
+++ database/models/qa_commands.js
@@ -1,1 +1,51 @@
-let QAModel = require('./qa')
+let QA = require('./qa')
+
+
+
+// let questionsSchema = new mongoose.Schema({
+//   title: {type: String, required: true, unique: true},
+//   text: {type: String, required: true},
+//   date: { type: Date, default: Date.now },
+//   author: mongoose.ObjectId,
+//   answers: []
+// })
+
+// let answersSchema = new mongoose.Schema({
+//   text: {type: String, required: true},
+//   date: { type: Date, default: Date.now },
+//   author: mongoose.ObjectId,
+//   question: mongoose.ObjectId
+// })
+
+// module.exports = {
+//   questionsModel: mongoose.model('Questions', questionsSchema),
+//   answersModel: mongoose.model('Answers', answersSchema)
+// }
+
+let mongoose = require('mongoose')
+
+
+let idToQuery = "4ecc05e55dd98a436ddcc47c";
+idToQuery = mongoose.Types.ObjectId(idToQuery)
+
+let q_Id = "6125b4b858dd643905644cfd"
+q_Id = mongoose.Types.ObjectId(q_Id)
+
+const createQ = function(username, email, bio){
+  let question = new QA.questionsModel({
+    title: 'first question',
+    text: 'question text',
+    author: idToQuery
+  })
+  question.save()
+  .then(doc => {
+    console.log(doc)
+    // callback(null, doc)
+  })
+  .catch(err => {
+    console.log(err)
+  //  callback(err, null)
+  })
+}
+
+// createQ()
